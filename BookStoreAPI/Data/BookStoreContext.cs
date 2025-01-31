4423f2d828f2ce4bb764807d11e72bc1b87e21d2
rajneesh2023 <rajp99360@gmail.com>
2025-01-30 19:51:45 -0600

    Initial commit for .NET core backend

---
+++ BookStoreAPI/Data/BookStoreContext.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStoreAPI.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using BookStoreAPI.Models;
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options) { }
+        public DbSet<Book> Books { get; set;}
+    }
+}
---
+++ BookStoreAPI/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿namespace BookStoreAPI.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="The Title field is required.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage ="The Author field is required.")]
+        public string Author { get; set; }
+
+        [Range(0.01, 10000.00, ErrorMessage ="The price must be between 0.01 to 10000.00.")]
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; }
+
+    }
+}
